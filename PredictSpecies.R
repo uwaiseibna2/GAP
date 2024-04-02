29fc0ca733e996111ec347126a0d8cb68530722a
Uwaise Ibna Islam <117959486+uwaiseibna2@users.noreply.github.com>
2024-04-02 18:16:50 -0400

    Update PredictSpecies.R

--- PredictSpecies.R
+++ PredictSpecies.R
@@ -114,7 +114,7 @@ get_data<-function(transcript_id,path_status,path_file,tree_flag,path_tree){
     de<-data.frame(dendo_tree)
     unique_cols <- sapply(de, function(x) length(unique(x))) == 1
     de<- de[, !unique_cols]
-    de<-de[match(de$species_name,merged$species_name),]
+    de<-de[match(merged$species_name,de$species_name),]
     de<-de[,-c(1)]
     data<-cbind(data,de)
   }
